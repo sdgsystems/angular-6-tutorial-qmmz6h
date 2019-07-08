38bdcec628bc9c3cbab0d34d8953eb3ba41d7531
sdgsystems <sdgsystems@outlook.com>
2019-07-08 14:29:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-6-tutorial-qmmz6h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-6-tutorial-qmmz6h)
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpClientModule ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
+  bootstrap:    [ AppComponent ],
+  providers: [HeroService, MessageService]
+})
+export class AppModule { }
