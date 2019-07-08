62bc234b6cfaee667031c043894ad16c3b493677
sdgsystems <sdgsystems@outlook.com>
2019-07-08 15:49:38 -0500

    1 file modified

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,7 +3,7 @@ import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+// import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 const httpOptions = {
